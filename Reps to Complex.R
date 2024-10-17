5026819d93b0c1095c896682d78f2c8e99c1cb87
Nkechi Nnadi <nnadinke@msu.edu>
2024-10-17 14:02:19 -0400

    Update Reps to Complex.R
    Combined "VLTC.R" and "AddCycles.R" and "CancelPairs.R" and "Nonredundant.R" into one big script, all culminating in the "Reps to Complex.R" routine

--- Reps to Complex.R
+++ Reps to Complex.R
@@ -1,8 +1,108 @@
+#Vertex List to Cycle Function
+
+VLTC<-function(V) {
+k<-nrow(V)
+m<-ncol(V)
+
+A<-array(dim=c(m,k,2))
+for (i in 1:m-1){
+A[i,1,]<-V[,i]
+A[i,2,]<-V[,i+1]
+}
+A[m,1,]<-V[,m]
+A[m,2,]<-V[,1]
+
+return(A)
+}
+
+#FUNCTION "nonredundant" TO RETURN NON-REDUNDANT LIST OF VERTICES
+#Input argument is a 3D array, called "D"
+ 
+#source("VLTC.R")
+nonredundant<-function(D) {
+p<-dim(D)[1] #store the number of vertices in D as p
+d<-dim(D)[3] #dimension of ambient space is d
+
+#Get non-redundant list of vertices from this cycle rep/array
+#Initialize an empty 2p by d matrix, and fill it with entries from D
+
+V1<-matrix(,nrow=2*p, ncol=d)#initialize an empty matrix
+
+for (k in 1:p){
+V1[2*k-1,] <- D[k,1,]
+V1[2*k,] <- D[k,2,]
+ }
+
+V<-unique(V1)#function "unique" is only applicable to matrices
+#It eliminates repeated rows
+
+return(V)#non-redundant list of vertices
+}
+
+#Script to cancel repeated rows in an array by pairs
+
+#source("VLTC.R")
+CancelPairs<-function(E){
+F<-E #Copy E
+i<-1
+	while(i<(dim(F)[1])){
+		for(j in (i+1):dim(F)[1]){
+		#Look for a duplicate of row i
+		#print(c(i,j))
+		if(identical(F[i,,1],F[j,,1])==TRUE && identical(F[i,,2],F[j,,2])==TRUE){
+			F<-F[-c(i,j),,]
+			print("cancelled")
+			i<-i-1 #Do  not increment i
+			break
+			}
+	
+		}
+		i<-i+1	}
+return(F)
+}
+
+#Script to add two cycles together and cancel repeated edges
+
+#source("CancelPairs.R")
+
+AddCycles<-function(C,D){
+#Check that the two cycles are compatible/have same size:
+if(!dim(C)[2]==dim(D)[2]||!dim(C)[3]==dim(D)[3]){
+print("Cannot add arrays of different sizes!")
+return(-1)
+}
+
+#Otherwise, if they are compatible,
+else{
+d1<-dim(C)[1]+dim(D)[1]
+d2<-dim(C)[2]
+d3<-dim(C)[3]
+
+#Create an empty d1*d2*d3 array which will be the concatenation of C and D
+E<-array(dim=c(d1,d2,d3))
+
+#Fill in the entries of array E with the rows of C and D
+for(j in 1:d2){
+	for(k in 1:d3){
+		for(i in 1:dim(C)[1]){
+			E[i,j,k]<-C[i,j,k]
+					    }
+		for(i in 1:dim(D)[1]){
+			E[i+dim(C)[1],j,k]<-D[i,j,k] 
+					    }
+			}
+	}
+
+return(CancelPairs(E))}
+}
+
+
+
 #Script to make cycle representatives into complexes
 
 #source("VLTC.R")
-source("Nonredundant.R")
-source("CancelPairs.R")
+#source("Nonredundant.R")
+#source("CancelPairs.R")
 #source("AddCycles.R")
 
 #Initial input is a H_1 cycle representative, a p*2*d array D
